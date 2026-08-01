92a035c03ac8b3dc5c10486bbb7c8620be496dac
atlanticdg <104678431+atlanticdg@users.noreply.github.com>
2026-08-01 08:44:18 +0800

    Update diy-part1.sh

--- config/lede_master/diy-part1.sh
+++ config/lede_master/diy-part1.sh
@@ -10,6 +10,7 @@
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
+# sed -i '2i src-git small https://github.com/kenzok8/small-package' feeds.conf.default
 
 # other
 # rm -rf package/lean/{samba4,luci-app-samba4,luci-app-ttyd}
