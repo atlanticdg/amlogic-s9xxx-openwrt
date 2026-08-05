9ff36bcc54924018a6efc491a7fe803ac924ff36
atlanticdg <104678431+atlanticdg@users.noreply.github.com>
2026-08-05 08:42:11 +0800

    Update diy-part1.sh

--- config/immortalwrt_master/diy-part1.sh
+++ config/immortalwrt_master/diy-part1.sh
@@ -6,8 +6,11 @@
 # Source code repository: https://github.com/immortalwrt/immortalwrt / Branch: master
 #========================================================================================================================
 
-# Add a custom feed source
+# Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
+# sed -i '2i src-git small https://github.com/kenzok8/small-package' feeds.conf.default
 
 # Remove unnecessary packages
 # rm -rf package/emortal/{autosamba,ipv6-helper}
