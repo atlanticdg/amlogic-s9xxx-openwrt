3575073af248a84111cc9ca3f9e1a8fda6c0b6ee
atlanticdg <104678431+atlanticdg@users.noreply.github.com>
2026-08-18 15:24:36 +0800

    Add custom feed sources for Kenzo and Small

--- config/immortalwrt_master/diy-part1.sh
+++ config/immortalwrt_master/diy-part1.sh
@@ -8,6 +8,9 @@
 
 # Add a custom feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+# sed -i '$a src-git small https://github.com/kenzok8/small-package' feeds.conf.default # small 是稳定版，small-package 是追新版
 
 # Remove unnecessary packages
 # rm -rf package/emortal/{autosamba,ipv6-helper}
